9810d4b9b193b0f0e51b1c674d78e864dd9c00c3
johnDorian <jlessels@gmail.com>
2014-12-15 16:52:07 -0800

    removed dataset - to solve checking issues.

--- R/ggsnippets.R
+++ R/ggsnippets.R
@@ -4,23 +4,3 @@
 #' @docType package
 #' @import grid
 NULL
-
-#' Long term historical climate data for Breamar, UK
-#'
-#' A dataset containing monthly maximum, minimum temperature, rainfall and sunlight hours
-#'
-#' \itemize{
-#'   \item year. year of record
-#'   \item month. month of record
-#'   \item max_temp. maximum temperature in this month
-#'   \item min_temp. minimum temperature in this month
-#'   \item rain_mm. total rainfall of the month
-#'   \item sun_hours. total sunlight hours in the month
-#' }
-#' @source \url{http://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/braemardata.txt}
-#' @docType data
-#' @keywords datasets
-#' @name braemardata
-#' @usage data(braemardata)
-#' @format A data frame with 669 rows and 6 variables.
-NULL
