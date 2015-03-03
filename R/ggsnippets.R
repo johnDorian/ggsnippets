64c5938272c01f23ca7f61c7bcf250c8fcd03a13
johnDorian <jlessels@gmail.com>
2015-03-03 22:31:23 +0000

    Added documentation for the breamar dataset.

--- R/ggsnippets.R
+++ R/ggsnippets.R
@@ -5,3 +5,22 @@
 #' @import grid ggplot2 gridExtra digest
 
 NULL
+
+#' Climatic data used in the package's examples.
+#' 
+#' This dataset contans min and maximum temperature, rainfall and
+#' the amount of sunlight hours for each day.
+#' 
+#' \itemize{
+#'  \item year. The year of the sample
+#'  \item month. The month the sample was taken
+#'  \item max_temp. The maximum temperature
+#'  \item min_temp. The minimum temperature
+#'  \item rain_mm. The rainfall for the month.
+#'  \item sun_hours. The amount of sunlight hours for the month.
+#'  }
+#'  
+#'  @format A data frame with 669 rows and 6 variables
+#'  @name breamardata
+
+NULL
